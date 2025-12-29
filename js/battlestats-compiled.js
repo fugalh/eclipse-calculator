b1cc6bd10e4d3526a924299f5517f0d2d93b0447
Hans Fugal <hans@fugal.net>
2025-12-29 10:38:51 -0700

    formatted js with vscode

--- js/battlestats-compiled.js
+++ js/battlestats-compiled.js
@@ -27,35 +27,35 @@ function k(a, b) {
     if (0 == _.size(a))
         return [];
     var e = _.size(a)
-      , c = a[0];
+        , c = a[0];
     this.b = [];
     if (b) {
         for (d = 0; d < c.missiles_yellow * e * 2; d++)
-            this.b.push(new g("yellow",c,!0));
+            this.b.push(new g("yellow", c, !0));
         for (d = 0; d < c.missiles_orange * e * 2; d++)
-            this.b.push(new g("orange",c,!0));
+            this.b.push(new g("orange", c, !0));
         for (d = 0; d < c.missiles_red * e * 2; d++)
-            this.b.push(new g("red",c,!0))
+            this.b.push(new g("red", c, !0))
     } else {
         for (var d = 0; d < c.yellow * e; d++)
-            this.b.push(new g("yellow",c,!0));
+            this.b.push(new g("yellow", c, !0));
         for (d = 0; d < c.orange * e; d++)
-            this.b.push(new g("orange",c,!0));
-		for (d = 0; d < c.blue * e; d++)
-            this.b.push(new g("blue",c,!0));
+            this.b.push(new g("orange", c, !0));
+        for (d = 0; d < c.blue * e; d++)
+            this.b.push(new g("blue", c, !0));
         for (d = 0; d < c.red * e; d++)
-            this.b.push(new g("red",c,!0))
+            this.b.push(new g("red", c, !0))
     }
     c.splitter && !b && splitAm(this)
 }
 function splitAm(a) {
-    var b = _.filter(a.b, function(a) {
+    var b = _.filter(a.b, function (a) {
         return "red" == a.type
     });
     a.b = _.difference(a.b, b);
-    _.each(b, function(b) {
+    _.each(b, function (b) {
         for (var c = 0; 4 > c; c++) {
-            var d = new g("yellow",b.origin,!1);
+            var d = new g("yellow", b.origin, !1);
             d.value = b.value;
             a.b.push(d)
         }
@@ -63,28 +63,28 @@ function splitAm(a) {
 }
 function n(a, b) {
     function e() {
-        return _.find(c, function(a) {
-            return _.find(b, function(c) {
+        return _.find(c, function (a) {
+            return _.find(b, function (c) {
                 return c.isAlive() && h(a, c)
             })
         })
     }
-    var c = _.sortBy(a.b, function(a) {
+    var c = _.sortBy(a.b, function (a) {
         return 100 * a.value - a.h
     })
-      , d = _.sortBy(b, function(a) {
-        return -a.targetPriority.indexOf(a.shipClass)
-    });
-    for (e(); e(); ) {
-        var f = c.shift()
-          , m = _.find(d, function(a) {
-            return a.isAlive() && h(f, a)
-        })
-          , q = _.find(d, function(a) {
-            return a.isAlive() && p(a, _.union(c, [f])) && h(f, a)
+        , d = _.sortBy(b, function (a) {
+            return -a.targetPriority.indexOf(a.shipClass)
         });
+    for (e(); e();) {
+        var f = c.shift()
+            , m = _.find(d, function (a) {
+                return a.isAlive() && h(f, a)
+            })
+            , q = _.find(d, function (a) {
+                return a.isAlive() && p(a, _.union(c, [f])) && h(f, a)
+            });
         m && (q && (m = q),
-        r(m, f.h))
+            r(m, f.h))
     }
 }
 function combatant(a) {
@@ -93,7 +93,7 @@ function combatant(a) {
     this.hull = a.hull || 0;
     this.yellow = a.yellow || 0;
     this.orange = a.orange || 0;
-	this.blue = a.blue || 0;
+    this.blue = a.blue || 0;
     this.red = a.red || 0;
     this.missiles_yellow = a.missiles_yellow || 0;
     this.missiles_orange = a.missiles_orange || 0;
@@ -110,15 +110,15 @@ function r(a, b) {
     a.hp -= b;
     0 > a.hp && (a.hp = 0)
 }
-combatant.prototype.isAlive = function() {
+combatant.prototype.isAlive = function () {
     return 0 < this.hp
 }
-;
+    ;
 function p(a, b) {
-    var e = _.filter(b, function(c) {
+    var e = _.filter(b, function (c) {
         return h(c, a)
     });
-    return _.reduce(e, function(a, b) {
+    return _.reduce(e, function (a, b) {
         return a + b.h
     }, 0) >= a.hp
 }
@@ -127,72 +127,72 @@ function t(ship, b) {
     for (var e = ship.number || 1, c = 0; c < e; c++) {
         var d = new combatant(ship);
         b ? (d.initiative += 0.1,
-        d.f = "D") : d.f = "A";
+            d.f = "D") : d.f = "A";
         this.a.push(d)
     }
 }
-t.prototype.isEmpty = function() {
+t.prototype.isEmpty = function () {
     return !this.a || 0 == _.size(this.a)
 }
-;
+    ;
 function u(a) {
     return a.isEmpty() ? void 0 : a.a[0]
 }
 function v(a) {
-    return _.filter(a.a, function(a) {
+    return _.filter(a.a, function (a) {
         return a.isAlive()
     })
 }
-t.prototype.c = function() {
+t.prototype.c = function () {
     return 0 < _.size(v(this))
 }
-;
+    ;
 function initiative(a, b) {
     var e = this;
     e.combatants = [];
-    _.each(a.ships, function(a) {
-        a = new t(a,!0);
+    _.each(a.ships, function (a) {
+        a = new t(a, !0);
         e.combatants.push(a)
     });
-    _.each(b.ships, function(a) {
-        e.combatants.push(new t(a,!1))
+    _.each(b.ships, function (a) {
+        e.combatants.push(new t(a, !1))
     });
-    e.combatants = _.sortBy(e.combatants, function(a) {
+    e.combatants = _.sortBy(e.combatants, function (a) {
         return -u(a).initiative
     })
 }
 function x(a) {
     var b = [];
-    _.each(a.combatants, function(a) {
+    _.each(a.combatants, function (a) {
         b = _.union(b, a.a)
     });
     return b
 }
 function y(a, b) {
-    return _.filter(x(a), function(a) {
+    return _.filter(x(a), function (a) {
         return a.isAlive() && (a.f == b || !b)
     })
 }
-initiative.prototype.status = function() {
+initiative.prototype.status = function () {
     var a = {
         o: 0 < _.size(y(this, "A")),
         k: 0 < _.size(y(this, "D")),
         C: _.size(y(this)),
         a: {}
     }
-      , b = _.find(y(this), function(a) {
-        return 0 < a.yellow || 0 < a.orange || 0 < a.red
-    });
+        , b = _.find(y(this), function (a) {
+            return 0 < a.yellow || 0 < a.orange || 0 < a.red
+        });
     a.j = !a.o || !a.k || !b;
     a.j && (a.d = a.k ? "D" : "A");
     if (a.j) {
-        var b = _.countBy(y(this), function(c) {
+        var b = _.countBy(y(this), function (c) {
             return c.f == a.d ? c.name : ""
         })
-          , e = _.countBy(x(this), function(c) {
-            return c.f == a.d ? c.name : ""
-        });
-        _.each(b, function(c, b) {
+            , e = _.countBy(x(this), function (c) {
+                return c.f == a.d ? c.name : ""
+            });
+        _.each(b, function (c, b) {
             var f = e[b];
             f && 0 != f || (f = 1);
             "" != b && (a.a[b] = c / f)
@@ -200,28 +200,28 @@ initiative.prototype.status = function() {
     }
     return a
 }
-;
+    ;
 function resolveBattle(defenseFleet, attackFleet) {
     var e = {
         d: "",
         a: {}
     }
-      , fireOrder = new initiative(defenseFleet,attackFleet);
+        , fireOrder = new initiative(defenseFleet, attackFleet);
     combatRound(fireOrder, !0);
-    for (var d = fireOrder.status(); !d.j; )
+    for (var d = fireOrder.status(); !d.j;)
         combatRound(fireOrder, !1),
-        d = fireOrder.status();
+            d = fireOrder.status();
     e.d = d.d;
     e.a = d.a;
     return e
 }
 function combatRound(fireOrder, missileOnly) {
     var e = x(fireOrder);
-    _.each(fireOrder.combatants, function(combatant) {
+    _.each(fireOrder.combatants, function (combatant) {
         if (combatant.c()) {
             var d = u(combatant).f;
-            combatant = new k(v(combatant),missileOnly);
-            var f = _.filter(e, function(a) {
+            combatant = new k(v(combatant), missileOnly);
+            var f = _.filter(e, function (a) {
                 return a.f != d && a.isAlive()
             });
             n(combatant, f)
@@ -230,13 +230,13 @@ function combatRound(fireOrder, missileOnly) {
 }
 function B(a) {
     var b = {};
-    _.each(a, function(a) {
-        _.each(a.a, function(a, c) {
+    _.each(a, function (a) {
+        _.each(a.a, function (a, c) {
             b[c] = b[c] ? b[c] + a : a
         })
     });
     var e = {};
-    _.each(b, function(b, d) {
+    _.each(b, function (b, d) {
         e[d] = b / _.size(a)
     });
     return e
@@ -252,10 +252,10 @@ function calculate(defenseFleet, attackFleet, iterations) {
         var battle = resolveBattle(defenseFleet, attackFleet);
         results.push(battle)
     }
-    defenseFleet = _.filter(results, function(defenseFleet) {
+    defenseFleet = _.filter(results, function (defenseFleet) {
         return "D" == defenseFleet.d
     });
-    attackFleet = _.filter(results, function(defenseFleet) {
+    attackFleet = _.filter(results, function (defenseFleet) {
         return "A" == defenseFleet.d
     });
     res.attacker = _.size(attackFleet) / _.size(results);
@@ -264,7 +264,7 @@ function calculate(defenseFleet, attackFleet, iterations) {
     res.shipsDefender = B(defenseFleet);
     return res
 }
-window.AnalyzeBattle = function(defenseFleet, attackFleet, iterations) {
+window.AnalyzeBattle = function (defenseFleet, attackFleet, iterations) {
     return calculate(defenseFleet, attackFleet, iterations)
 }
-;
+    ;
--- js/ui-compiled.js
+++ js/ui-compiled.js
@@ -14,7 +14,7 @@
 var e = {
     yellow: [0, 6],
     orange: [0, 6],
-	blue: [0, 6],
+    blue: [0, 6],
     red: [0, 6],
     missiles_yellow: [0, 6],
     missiles_orange: [0, 6],
@@ -25,88 +25,88 @@ var e = {
     initiative: [0, 8],
     number: [1, 6]
 }
-  , m = [{
-    yellow: 0,
-    computers: 0,
-    shields: 0,
-    hull: 0,
-    initiative: 0,
-    shipClass: "Cruiser",
-    name: "Generic ship",
-    type: "generic"
-}, {
-    yellow: 4,
-    computers: 1,
-    shields: 0,
-    hull: 7,
-    initiative: 0,
-    shipClass: "GC",
-    name: "Galactic Center",
-    type: "npc"
-}, {
-    yellow: 2,
-    computers: 1,
-    shields: 0,
-    hull: 1,
-    initiative: 2,
-    shipClass: "Ancient",
-    name: "Ancient",
-    type: "npc"
-}, {
-    yellow: 1,
-    computers: 0,
-    shields: 0,
-    hull: 0,
-    initiative: 3,
-    shipClass: "Interceptor",
-    name: "Interceptor",
-    type: "player"
-}, {
-    yellow: 1,
-    computers: 1,
-    shields: 0,
-    hull: 1,
-    initiative: 2,
-    shipClass: "Cruiser",
-    name: "Cruiser",
-    type: "player"
-}, {
-    yellow: 2,
-    computers: 1,
-    shields: 0,
-    hull: 2,
-    initiative: 1,
-    shipClass: "Dreadnought",
-    name: "Dreadnought",
-    type: "player"
-}, {
-    yellow: 1,
-    computers: 1,
-    shields: 0,
-    hull: 2,
-    initiative: 4,
-    shipClass: "Starbase",
-    name: "Starbase",
-    type: "player"
-},{
-    yellow: 2,
-    computers: 0,
-    shields: 0,
-    hull: 3,
-    initiative: 0,
-    shipClass: "Orbital",
-    name: "Exile Orbital",
-    type: "player"
-},{
-    red: 1,
-    computers: 1,
-    shields: 0,
-    hull: 4,
-    initiative: 1,
-    shipClass: "Deathmoon",
-    name: "Deathmoon",
-    type: "player"
-}];
+    , m = [{
+        yellow: 0,
+        computers: 0,
+        shields: 0,
+        hull: 0,
+        initiative: 0,
+        shipClass: "Cruiser",
+        name: "Generic ship",
+        type: "generic"
+    }, {
+        yellow: 4,
+        computers: 1,
+        shields: 0,
+        hull: 7,
+        initiative: 0,
+        shipClass: "GC",
+        name: "Galactic Center",
+        type: "npc"
+    }, {
+        yellow: 2,
+        computers: 1,
+        shields: 0,
+        hull: 1,
+        initiative: 2,
+        shipClass: "Ancient",
+        name: "Ancient",
+        type: "npc"
+    }, {
+        yellow: 1,
+        computers: 0,
+        shields: 0,
+        hull: 0,
+        initiative: 3,
+        shipClass: "Interceptor",
+        name: "Interceptor",
+        type: "player"
+    }, {
+        yellow: 1,
+        computers: 1,
+        shields: 0,
+        hull: 1,
+        initiative: 2,
+        shipClass: "Cruiser",
+        name: "Cruiser",
+        type: "player"
+    }, {
+        yellow: 2,
+        computers: 1,
+        shields: 0,
+        hull: 2,
+        initiative: 1,
+        shipClass: "Dreadnought",
+        name: "Dreadnought",
+        type: "player"
+    }, {
+        yellow: 1,
+        computers: 1,
+        shields: 0,
+        hull: 2,
+        initiative: 4,
+        shipClass: "Starbase",
+        name: "Starbase",
+        type: "player"
+    }, {
+        yellow: 2,
+        computers: 0,
+        shields: 0,
+        hull: 3,
+        initiative: 0,
+        shipClass: "Orbital",
+        name: "Exile Orbital",
+        type: "player"
+    }, {
+        red: 1,
+        computers: 1,
+        shields: 0,
+        hull: 4,
+        initiative: 1,
+        shipClass: "Deathmoon",
+        name: "Deathmoon",
+        type: "player"
+    }];
 function F(a) {
     this.D = {};
     H(this, a || {});
@@ -115,7 +115,7 @@ function F(a) {
 function H(a, c) {
     a.D.yellow = c.yellow || 0;
     a.D.orange = c.orange || 0;
-	a.D.blue = c.blue || 0;
+    a.D.blue = c.blue || 0;
     a.D.red = c.red || 0;
     a.D.missiles_yellow = c.missiles_yellow || 0;
     a.D.missiles_orange = c.missiles_orange || 0;
@@ -150,7 +150,7 @@ function J(a) {
         a.F.D = {};
         a.F.D.J = $("<ul></ul>").addClass("specs");
         a.F.H.append(a.F.D.J);
-        _.each("yellow orange blue red missiles_yellow missiles_orange number computers shields hull initiative".split(" "), function(c) {
+        _.each("yellow orange blue red missiles_yellow missiles_orange number computers shields hull initiative".split(" "), function (c) {
             var q = a.F.D, f;
             f = $("<li></li>").addClass(c).data({
                 name: c
@@ -165,7 +165,7 @@ function J(a) {
             splitter: "Antimatter Splitter",
             missile_shield: "Distortion Field"
         };
-        _.each(_.keys(c), function(d) {
+        _.each(_.keys(c), function (d) {
             var q = a.F.I, f;
             f = c[d];
             f = $("<li></li>").addClass(d).data({
@@ -188,7 +188,7 @@ function I(a) {
     a.F.shipClass.text(a.D.shipClass);
     K(a, "yellow", a.D.yellow);
     K(a, "orange", a.D.orange);
-	K(a, "blue", a.D.blue);
+    K(a, "blue", a.D.blue);
     K(a, "red", a.D.red);
     K(a, "missiles_yellow", a.D.missiles_yellow);
     K(a, "missiles_orange", a.D.missiles_orange);
@@ -200,8 +200,8 @@ function I(a) {
     K(a, "number", a.D.number);
     O(a, "missile_shield", a.D.missile_shield);
     O(a, "splitter", a.D.splitter);
-    O(a, "point_defense", 
-    a.D.L)
+    O(a, "point_defense",
+        a.D.L)
 }
 function P(a) {
     return {
@@ -211,7 +211,7 @@ function P(a) {
         hull: a.D.hull,
         yellow: a.D.yellow,
         orange: a.D.orange,
-		blue: a.D.blue,
+        blue: a.D.blue,
         red: a.D.red,
         missiles_yellow: a.D.missiles_yellow,
         missiles_orange: a.D.missiles_orange,
@@ -235,9 +235,9 @@ function R(a) {
     if (c)
         try {
             a.G = JSON.parse(c),
-            a.G = _.filter(a.G, function(a) {
-                return a.name
-            })
+                a.G = _.filter(a.G, function (a) {
+                    return a.name
+                })
         } catch (d) {
             a.G = []
         }
@@ -249,36 +249,36 @@ function S(a) {
     })
 }
 function T(a, c, d) {
-    var q = _.find(a.G, function(a) {
+    var q = _.find(a.G, function (a) {
         return a.name.toLowerCase() == c.toLowerCase()
     });
     q && (a.G = _.without(a.G, q),
-    S(a),
-    d.O())
+        S(a),
+        d.O())
 }
 function U(a) {
     var c = $("div.presets ul").empty();
     _.union(a.K, a.G);
-    _.each(a.K, function(a) {
+    _.each(a.K, function (a) {
         a = $("<li></li>").addClass(a.type).addClass(a.shipClass.toLowerCase()).attr("rel", a.name).append($('<div class="icon"></div>').append($("<span></span>").text(a.name)));
         c.append(a)
     });
-    _.each(a.G, function(a) {
+    _.each(a.G, function (a) {
         a = $("<li></li>").addClass("custom").addClass(a.shipClass.toLowerCase()).attr("rel", a.name).append($('<div class="icon"></div>').append($("<span></span>").text(a.name))).append($("<div>&ndash;</div>").addClass("remove"));
         c.append(a)
     })
 }
 function V(a, c) {
     if (c) {
-        var d = _.find(a.G, function(a) {
+        var d = _.find(a.G, function (a) {
             return a.name.toLowerCase() == c.toLowerCase()
         });
-        return d ? d : _.find(a.K, function(a) {
+        return d ? d : _.find(a.K, function (a) {
             return a.name.toLowerCase() == c.toLowerCase()
         })
     }
 }
-jQuery.fn.O = function() {
+jQuery.fn.O = function () {
     this.animate({
         width: "hide",
         paddingLeft: "hide",
@@ -288,10 +288,10 @@ jQuery.fn.O = function() {
         opacity: 0
     }, 100, void 0)
 }
-;
-$(function() {
+    ;
+$(function () {
     function a(a) {
-        a.slideUp("swing", function() {
+        a.slideUp("swing", function () {
             a.remove();
             L()
         })
@@ -305,16 +305,16 @@ $(function() {
         $("div.results .victorychance div").removeClass("winner");
         $("div.results .victorychance .defender .probability").text(Math.round(100 * a.defender) + "%");
         $("div.results .victorychance .attacker .probability").text(Math.round(100 * a.attacker) + "%");
-        a.attacker > a.defender ? $("div.results .victorychance div.attacker").addClass("winner") : 
-        $("div.results .victorychance div.defender").addClass("winner");
+        a.attacker > a.defender ? $("div.results .victorychance div.attacker").addClass("winner") :
+            $("div.results .victorychance div.defender").addClass("winner");
         $("div.results div.survival ul").empty();
-        _.each(a.shipsAttacker, function(a, c) {
+        _.each(a.shipsAttacker, function (a, c) {
             var D = $('<li><div class="class"></div><div class="name"></div><div class="probabilities"><span class="chance1"></span></div></li>');
             D.find(".name").text(c);
             D.find(".probabilities .chance1").text(Math.round(100 * a) + "%");
             $("div.results div.survival#A ul").append(D)
         });
-        _.each(a.shipsDefender, function(a, D) {
+        _.each(a.shipsDefender, function (a, D) {
             var c = $('<li><div class="class"></div><div class="name"></div><div class="probabilities"><span class="chance1"></span></div></li>');
             c.find(".name").text(D);
             c.find(".probabilities .chance1").text(Math.round(100 * a) + "%");
@@ -335,58 +335,58 @@ $(function() {
     }
     function M(D) {
         var b = $("div.presets")
-          , d = b.data().N
-          , b = b.data().M
-          , f = V(E, D);
+            , d = b.data().N
+            , b = b.data().M
+            , f = V(E, D);
         d && D && f ? (H(d, f),
-        I(d),
-        c()) : b && a(d.F.H)
+            I(d),
+            c()) : b && a(d.F.H)
     }
     function W(a) {
         setTimeout(a, 0.2)
     }
     function L() {
-        0 < 
-        $("div#shipsD ul.ships li.ship").length && 0 < $("div#shipsA ul.ships li.ship").length ? $("a.simulate").removeClass("disabled") : $("a.simulate").addClass("disabled")
+        0 <
+            $("div#shipsD ul.ships li.ship").length && 0 < $("div#shipsA ul.ships li.ship").length ? $("a.simulate").removeClass("disabled") : $("a.simulate").addClass("disabled")
     }
     var E = new Q;
     U(E);
-    $(document.body).on("click", "ul.specs li", function() {
+    $(document.body).on("click", "ul.specs li", function () {
         var a = $(this)
-          , b = a.parents("li.ship").data()
-          , a = a.data().name;
+            , b = a.parents("li.ship").data()
+            , a = a.data().name;
         "yellow" == a ? (b.D.yellow++,
-        b.D.yellow > e.yellow[1] && (b.D.yellow = e.yellow[0])) : "orange" == a ? (b.D.orange++,
-        b.D.orange > e.orange[1] && (b.D.orange = e.orange[0])) : "blue" == a ? (b.D.blue++,
-        b.D.blue > e.blue[1] && (b.D.blue = e.blue[0])) : "red" == a ? (b.D.red++,
-        b.D.red > e.red[1] && (b.D.red = e.red[0])) : "missiles_yellow" == 
-        a ? (b.D.missiles_yellow++,
-        b.D.missiles_yellow > e.missiles_yellow[1] && (b.D.missiles_yellow = e.missiles_yellow[0])) : "missiles_orange" == a ? (b.D.missiles_orange++,
-        b.D.missiles_orange > e.missiles_orange[1] && (b.D.missiles_orange = e.missiles_orange[0])) : "missiles_red" == a ? (b.D.missiles_red++,
-        b.D.missiles_red > e.missiles_red[1] && (b.D.missiles_red = e.missiles_red[0])) : "computers" == a ? (b.D.computers++,
-        b.D.computers > e.computers[1] && (b.D.computers = e.computers[0])) : "shields" == a ? (b.D.shields++,
-        b.D.shields > e.shields[1] && 
-        (b.D.shields = e.shields[0])) : "hull" == a ? (b.D.hull++,
-        b.D.hull > e.hull[1] && (b.D.hull = e.hull[0])) : "initiative" == a ? (b.D.initiative++,
-        b.D.initiative > e.initiative[1] && (b.D.initiative = e.initiative[0])) : "number" == a && (b.D.number++,
-        b.D.number > e.number[1] && (b.D.number = e.number[0]));
+            b.D.yellow > e.yellow[1] && (b.D.yellow = e.yellow[0])) : "orange" == a ? (b.D.orange++,
+                b.D.orange > e.orange[1] && (b.D.orange = e.orange[0])) : "blue" == a ? (b.D.blue++,
+                    b.D.blue > e.blue[1] && (b.D.blue = e.blue[0])) : "red" == a ? (b.D.red++,
+                        b.D.red > e.red[1] && (b.D.red = e.red[0])) : "missiles_yellow" ==
+                            a ? (b.D.missiles_yellow++,
+                                b.D.missiles_yellow > e.missiles_yellow[1] && (b.D.missiles_yellow = e.missiles_yellow[0])) : "missiles_orange" == a ? (b.D.missiles_orange++,
+                                    b.D.missiles_orange > e.missiles_orange[1] && (b.D.missiles_orange = e.missiles_orange[0])) : "missiles_red" == a ? (b.D.missiles_red++,
+                                        b.D.missiles_red > e.missiles_red[1] && (b.D.missiles_red = e.missiles_red[0])) : "computers" == a ? (b.D.computers++,
+                                            b.D.computers > e.computers[1] && (b.D.computers = e.computers[0])) : "shields" == a ? (b.D.shields++,
+                                                b.D.shields > e.shields[1] &&
+                                                (b.D.shields = e.shields[0])) : "hull" == a ? (b.D.hull++,
+                                                    b.D.hull > e.hull[1] && (b.D.hull = e.hull[0])) : "initiative" == a ? (b.D.initiative++,
+                                                        b.D.initiative > e.initiative[1] && (b.D.initiative = e.initiative[0])) : "number" == a && (b.D.number++,
+                                                            b.D.number > e.number[1] && (b.D.number = e.number[0]));
         I(b);
         c(!0);
         return !1
     });
-    $(document.body).on("click", "ul.toggles li", function() {
+    $(document.body).on("click", "ul.toggles li", function () {
         var a = $(this)
-          , b = a.parents("li.ship").data()
-          , a = a.data().name;
-        "splitter" == a ? b.D.splitter = !b.D.splitter : "missile_shield" == a ? b.D.missile_shield = !b.D.missile_shield : 
-        "point_defense" == a && (b.D.L = !b.D.L);
+            , b = a.parents("li.ship").data()
+            , a = a.data().name;
+        "splitter" == a ? b.D.splitter = !b.D.splitter : "missile_shield" == a ? b.D.missile_shield = !b.D.missile_shield :
+            "point_defense" == a && (b.D.L = !b.D.L);
         I(b);
         c(!0);
         return !1
     });
-    $(document.body).on("click", "a.addship", function() {
+    $(document.body).on("click", "a.addship", function () {
         var a = $(this).parents("div.ships").find("ul.ships")
-          , b = new F(V(E, "Generic ship"));
+            , b = new F(V(E, "Generic ship"));
         a.append(b.F.H);
         b.F.H.hide().slideDown("swing");
         q(b, !0);
@@ -394,25 +394,25 @@ $(function() {
         L();
         return !1
     });
-    $(document.body).on("click", "a.remove", function() {
+    $(document.body).on("click", "a.remove", function () {
         var D = $(this).parents("li.ship");
         a(D);
         c(!0);
         return !1
     });
-    $(document.body).on("click", "a.templates", 
-    function() {
-        var a = $(this).parents("li.ship").data();
-        q(a, !1);
-        return !1
-    });
-    $(document.body).on("click", "a.save", function() {
+    $(document.body).on("click", "a.templates",
+        function () {
+            var a = $(this).parents("li.ship").data();
+            q(a, !1);
+            return !1
+        });
+    $(document.body).on("click", "a.save", function () {
         var a = $(this).parents("li.ship").data()
-          , b = window.prompt("Name the preset", "Ship " + Math.ceil(1E3 * Math.random()));
+            , b = window.prompt("Name the preset", "Ship " + Math.ceil(1E3 * Math.random()));
         if (!b)
             return !1;
         var c = E
-          , d = P(a);
+            , d = P(a);
         d.name = b;
         c.G.push(d);
         S(c);
@@ -422,50 +422,50 @@ $(function() {
         q(void 0, !1);
         return !1
     });
-    $(document.body).on("click", "a.simulate", function() {
+    $(document.body).on("click", "a.simulate", function () {
         var attackFleet = {
             ships: []
         };
         var defenseFleet = {
             ships: []
         };
-        $("div#shipsA ul.ships li.ship").each(function(defenseFleet, ship) {
+        $("div#shipsA ul.ships li.ship").each(function (defenseFleet, ship) {
             attackFleet.ships.push(P($(ship).data()))
         });
-        $("div#shipsD ul.ships li.ship").each(function(attackFleet, ship) {
+        $("div#shipsD ul.ships li.ship").each(function (attackFleet, ship) {
             defenseFleet.ships.push(P($(ship).data()))
         });
-        _.size(defenseFleet.ships) && _.size(attackFleet.ships) && $(".lightbox#working").fadeIn("swing", function() {
-            W(function() {
+        _.size(defenseFleet.ships) && _.size(attackFleet.ships) && $(".lightbox#working").fadeIn("swing", function () {
+            W(function () {
                 var c = window.AnalyzeBattle(defenseFleet, attackFleet, 1000);
                 d(c);
-                $(".lightbox#working").fadeOut("swing", function() {})
+                $(".lightbox#working").fadeOut("swing", function () { })
             })
         });
         return !1
     });
-    $(document.body).on("click", "div.presets li", function(a) {
+    $(document.body).on("click", "div.presets li", function (a) {
         var b = $(this).attr("rel");
         M(b);
         f();
         a.stopPropagation()
     });
-    $(document.body).on("click", "div.presets li div.remove", function(a) {
+    $(document.body).on("click", "div.presets li div.remove", function (a) {
         var b = $(this).parents("li")
-          , c = b.attr("rel");
+            , c = b.attr("rel");
         T(E, c, b);
         a.stopPropagation()
     });
-    $(document.body).on("click", ".lightbox#presets", function() {
+    $(document.body).on("click", ".lightbox#presets", function () {
         M("none");
         f()
     });
-    $(document.body).on("click", "a.showdesc", function() {
+    $(document.body).on("click", "a.showdesc", function () {
         $("div.intro div.description").slideDown();
         $("a.showdesc").fadeOut();
         return !1
     });
-    $(document.body).on("click", "div.presets a.close", function() {
+    $(document.body).on("click", "div.presets a.close", function () {
         M("none");
         f();
         return !1
@@ -474,7 +474,7 @@ $(function() {
     $(".lightbox").hide();
     $("a.showdesc").hide();
     var N = $("div.ships#shipsD ul.ships")
-      , G = new F(V(E, "Ancient"));
+        , G = new F(V(E, "Ancient"));
     N.append(G.F.H);
     G.F.H.hide().slideDown("swing");
     N = $("div.ships#shipsA ul.ships");
